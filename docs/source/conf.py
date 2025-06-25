eac14c560049db72aa5ebed2aae3ec6d511c4a2d
cardigan1008 <ybni@cse.cuhk.edu.hk>
2025-06-26 00:12:16 +0800

    fix: update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LegoFuzz'
+copyright = '2025, Yunbo Ni'
+author = 'Yunbo Ni'
 
 release = '0.1'
 version = '0.1.0'
